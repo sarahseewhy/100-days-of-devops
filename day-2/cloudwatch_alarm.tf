c00c645db51752848f39ce5ac7e0f08c5c9e1f72
Sarah C Y <sarahseewhy@gmail.com>
2019-07-11 12:07:42 +0100

    feat(day-2): Move day 2 task to its own directory
    * Accidentally did day 2 task as part of day 1.

---
+++ day-2/cloudwatch_alarm.tf
@@ -0,0 +1,22 @@
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization" {
+  alarm_name = "high_cpu_utilization"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = "2"
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = "120"
+  statistic = "Average"
+  threshold = "70"
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+
+  dimensions = {
+    InstanceId = aws_instance.web.id
+  }
+
+  tags = {
+    env     = "spike"
+    service = "100-days-devops"
+    name    = "hello-world"
+  }
+}
